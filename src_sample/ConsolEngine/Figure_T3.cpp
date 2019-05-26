f2b4b4091b8fd35a871b1c2789d5bdd3cfbcb421
Mamoru <32417036+Drift2020@users.noreply.github.com>
2019-05-26 16:16:41 +0300

    Update Figure_T3.cpp

--- src_sample/ConsolEngine/Figure_T3.cpp
+++ src_sample/ConsolEngine/Figure_T3.cpp
@@ -14,13 +14,13 @@ Figure_T3::Figure_T3() : Figure_Parent()
 	_blocks = new Block*[_size];
 
 
-	_blocks[0] = new  Block(1, 1, '&', my_enums::Down);;
+	_blocks[0] = new  Block(1, 0, '&', my_enums::Down);;
 
-	_blocks[1] = new  Block(0, 0, '&', my_enums::Down);
+	_blocks[1] = new  Block(0, 1, '&', my_enums::Down);
 
-	_blocks[2] = new  Block(1, 0, '&', my_enums::Down);
+	_blocks[2] = new  Block(1, 1, '&', my_enums::Down);
 
-	_blocks[3] = new Block(2, 0, '&', my_enums::Down);
+	_blocks[3] = new Block(2, 1, '&', my_enums::Down);
 	_state = my_enums::Down;
 }
 
@@ -292,16 +292,16 @@ void Figure_T3::Rotate()
 		_state_rotate = my_enums::UP;
 
 		_blocks[0]->Set_X(0);
-		_blocks[0]->Set_Y(0);
+		_blocks[0]->Set_Y(1);
 
 		_blocks[1]->Set_X(1);
-		_blocks[1]->Set_Y(0);
+		_blocks[1]->Set_Y(1);
 
 		_blocks[2]->Set_X(2);
-		_blocks[2]->Set_Y(0);
+		_blocks[2]->Set_Y(1);
 
 		_blocks[3]->Set_X(1);
-		_blocks[3]->Set_Y(1);
+		_blocks[3]->Set_Y(2);
 
 
 		break;
@@ -311,16 +311,16 @@ void Figure_T3::Rotate()
 
 		_state_rotate = my_enums::Left;
 
-		_blocks[0]->Set_X(0);
+		_blocks[0]->Set_X(1);
 		_blocks[0]->Set_Y(0);
 
-		_blocks[1]->Set_X(0);
+		_blocks[1]->Set_X(1);
 		_blocks[1]->Set_Y(1);
 
-		_blocks[2]->Set_X(1);
+		_blocks[2]->Set_X(2);
 		_blocks[2]->Set_Y(1);
 
-		_blocks[3]->Set_X(0);
+		_blocks[3]->Set_X(1);
 		_blocks[3]->Set_Y(2);
 
 
